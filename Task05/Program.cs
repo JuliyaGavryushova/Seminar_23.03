e7555395be88e5ef1a38fa0bb6b256a30f3d7bc5
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-03-24 20:28:58 +0200

    Added Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿// 5. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count++;
+}
