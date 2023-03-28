86fbe40fd4bda5ed890aa259a41762d19cdc0c41
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-03-28 13:13:29 +0200

    Added Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿// 14. Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одеовременно 7 и 23
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsMultipleTwoNumbers(number);
+Console.WriteLine(result ? "Число кратно" : "Число не кратно");
+
+bool IsMultipleTwoNumbers(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
