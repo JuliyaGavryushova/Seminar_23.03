9c3f1d06f0df4e6a0a0dbaa37f038009199c3ec1
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-03-31 13:41:46 +0200

    Added Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿// 22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result = TableSquare(number);
+Console.Write(result);
+
+string TableSquare(int num)
+{
+    string tableSquareResult = "___________\n";
+    for (int i = 1; i <= num; i++)
+    {
+        tableSquareResult = tableSquareResult + $"|{i,3} | {i * i,3}| \n";
+    }
+    tableSquareResult = tableSquareResult + "-----------";
+    return tableSquareResult;
+}
+
+
