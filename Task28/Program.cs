857f41f835ecfb1e09db7f8016dfb1b11ddb3686
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-03 20:27:17 +0200

    Added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// 28. Напишите программу, которая принимает на вход число N и 
+// выдает произведение чисел от 1 до N
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int result = ProductNumbers(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} равно {result}");
+}
+else
+{
+    Console.WriteLine("Некорректное число!");
+}
+
+int ProductNumbers(int num)
+{
+    int product = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            product = product * i;
+        }
+    }
+    return product;
+}
