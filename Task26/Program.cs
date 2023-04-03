3f05a780b5efacb99bdb6b36bc33437d3190c6a4
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-03 15:37:39 +0200

    Added Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿// 26. Напишите программу, которая принимает на вход число
+// и выдает количество цифр в числе
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = HowManyDigits(number);
+Console.WriteLine($"В числе {result} цифр");
+
+int HowManyDigits(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
