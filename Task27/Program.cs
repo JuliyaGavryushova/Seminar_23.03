2fcd90ce620f845976f2579085280ae19a3251c8
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-04 10:22:45 +0200

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// 27. Напишите программу, которая принимает на вход число 
+// и выдает сумму цифр в числе
+
+Console.Write("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int result = SumDigits(number);
+Console.WriteLine($"Сумма цифр в числе => {result}");
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int oneDigit = num % 10;
+        sum = sum + oneDigit;
+        num = num / 10;
+    }
+    return sum;
+}
