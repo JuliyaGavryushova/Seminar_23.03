da4d55af431e50f2cf34eb2688073899d10466a3
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-06 21:33:16 +0200

    Added Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,50 @@
+﻿// 35. Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов
+// массива, значения которых лежат в отрезке [10,99]
+
+int[] array = CreateArrayRndInt(10, -100, 100);
+PrintArray(array);
+int quantity = FindElementsSegment(array);
+Console.WriteLine();
+Console.WriteLine($"{quantity} элемент(ов) лежит(ат) в отрезке [10,99]");
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length - 1)
+        {
+            Console.Write($"{arr[i]}");
+        }
+        else
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+    }
+    Console.Write("]");
+}
+
+int FindElementsSegment(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
