1a3ed4197730001cf7d81904258086ba77230b6d
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-10 20:59:50 +0200

    Added Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,38 @@
+﻿// 42. Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine($"Введите десятичное число: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+// string res = DecToBin(decimalNumber);
+// Console.WriteLine(res);
+
+int res = DecToBinWithIng(decimalNumber);
+Console.WriteLine(res);
+
+// string DecToBin(int dec)
+// {
+//     string temp = string.Empty;
+//     while (dec > 0)
+//     {
+//         temp = dec % 2 + temp;
+//         dec = dec / 2;
+//     }
+//     return temp;
+// }
+
+int DecToBinWithIng(int dec)
+{
+    int result = 0;
+    int count = 1;
+    while (dec > 0)
+    {
+        result = result + dec % 2 * count;
+        dec = dec / 2;
+        count = count * 10;
+    }
+    return result;
+}
