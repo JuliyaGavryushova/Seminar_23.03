db43eceb3595bbfd036f64d0d36d834cd4f952ce
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-13 15:52:53 +0200

    Added Task48

---
+++ Task48/Program.cs
@@ -0,0 +1,45 @@
+﻿// 48. Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] resultArray = CreateArrayMatrix(m, n);
+PrintArray(resultArray);
+
+int[,] CreateArrayMatrix(int rows, int colum)
+{
+    int[,] arr = new int[rows, colum];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i < arr.Length - 1) Console.Write($"{arr[i, j],3} ");
+            else Console.Write($"{arr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+
+}
