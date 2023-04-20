400d515ae0ecb4c5c00bbdbf60b786fa1ca2e470
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-20 15:27:25 +0200

    Added Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,20 @@
+﻿// 69. Напишите программу, которая на вход принимает два числа А и В,
+// и возводит число А в целую степень В с помощью рекурсии
+
+Console.WriteLine("Введите число A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB > 0)
+{
+    int result = Exponentiation(numberA, numberB);
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Некорректный ввод!");
+
+int Exponentiation(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    else return numA * Exponentiation(numA, numB - 1);
+}
