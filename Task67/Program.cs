5ee5adb556a061f4167534a36bff7d020c2e04ba
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-20 15:11:19 +0200

    Added Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// 67. Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumDigits(number);
+Console.WriteLine($"Сумма цифр в числе => {result}");
+
+int SumDigits(int num)
+{
+    if (num == 0) return 0;
+    else return num % 10 + SumDigits(num / 10);
+}
