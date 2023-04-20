a5982b843893cef1a50b9de71fdcffa88acd6d69
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-04-20 14:08:59 +0200

    Added Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,18 @@
+﻿// 63. Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа от 1 до N.
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    NaturalNumbers(number);
+}
+else Console.WriteLine("Ввели некорректное число");
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
